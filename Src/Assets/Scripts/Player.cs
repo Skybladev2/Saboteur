eec9345bd2ad4c4c996f2d4130630486dc2e2881
Skyladev2 <qlzp2@mail.ru>
2013-01-15 21:15:30 +0400

    Added some classes.

--- Src/Assets/Scripts/Player.cs
+++ Src/Assets/Scripts/Player.cs
@@ -1,15 +1,33 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    private byte order;
+    private string name;
+    private byte publicPlayerID;
+    private List<Card> goldNuggetCards = new List<Card>();
+    private Guid playerID;
+    private Card dwarfCard;
+    private PlayerHand playerhand;
+    private List<Card> brokenTools = new List<Card>();
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnPlayerRegistered(PlayerInfo playerInfo)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ Src/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerInfo : MonoBehaviour
+{
+    public Guid PlayerID;
+    public byte Order;
+    public Card[] GoldNuggetCards;
+    public PlayerHand PlayerHand;
+    public Card DwarfCard;
+    public Card[] BrokenTools;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
